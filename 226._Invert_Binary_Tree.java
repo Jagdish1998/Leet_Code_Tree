28ca2f402d2826049e228d1ba155c4e20ae927f8
Jagdish Sahoo <51652308+Jagdish1998@users.noreply.github.com>
2021-07-14 11:58:18 +0530

    Add files via upload

---
+++ 226._Invert_Binary_Tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void swap(TreeNode root){
+        if(root == null) return;
+        swap(root.left);
+        swap(root.right);
+        
+        TreeNode temp = new TreeNode();
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+    public TreeNode invertTree(TreeNode root) {
+        swap(root);
+        return root;
+    }
+}
